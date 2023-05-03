0c841a99db0030e3498f3e28a0a8d79ad151a154
Rares Macavei <raresmacavei15@gmail.com>
2023-05-03 12:33:33 +0300

    Header & Footer Components added

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './style.scss';
 
+import useFetch from '../../../hooks/useFetch';
+
+import Img from '../../../components/lazyLoadImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import Header from '../../../components/header/Header';
 
 const HeroBanner = () => {
-  const [background, setBackground] = useState("");
-  const [query, setQuery] = useState("");
+  const [background, setBackground] = useState('');
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+
+  const { data, loading } = useFetch('/movie/upcoming');
+
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path
+    setBackground(bg);
+  }, [data]);
 
   const searchQueryHandler = (event) => {
-    if (event.key === "Enter" && query.length > 0) {
+    if (event.key === 'Enter' && query.length > 0) {
       navigate(`/search/${query}`);
     }
   }
 
   return (
     <div className='heroBanner'>
-      <div className='wrapper'>
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+          <div className="opacity-layer"></div>
+        </div>
+      )}
+      <ContentWrapper>
         <div className='heroBannerContent'>
           <span className='title'>Welcome</span>
           <span className='subTitle'>
@@ -34,7 +54,8 @@ const HeroBanner = () => {
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </ContentWrapper>
+
     </div>
   );
 }
