33c05e630955800a8543e9a33c2ec3514cba16b9
Rares Macavei <raresmacavei15@gmail.com>
2023-05-10 19:27:21 +0300

    Debugging

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
+
 import "./style.scss";
-import HeroBanner from './heroBanner/HeroBanner';
-import Trending from './trending/Trending';
+
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 import Upcoming from './upcoming/Upcoming';
 import OnAir from './onAir/OnAir';
 
-
 const Home = () => {
   return (
-    <div className='homePage'>
+    <div className="homePage">
       <HeroBanner />
       <Trending />
+      <Popular />
+      <TopRated />
       <Upcoming />
       <OnAir />
-      <div style={{ height: 1000 }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
