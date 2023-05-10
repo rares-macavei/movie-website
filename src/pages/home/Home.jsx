c2dfccbb6cff84aa037fd2b3e78af431b43f2b5d
Rares Macavei <raresmacavei15@gmail.com>
2023-05-10 18:30:46 +0300

    Errors solved v1.1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import "./style.scss";
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
-import Popular from './popular/Popular';
-import TopRated from './topRated/TopRated';
 import Upcoming from './upcoming/Upcoming';
 import OnAir from './onAir/OnAir';
 
@@ -13,8 +11,6 @@ const Home = () => {
     <div className='homePage'>
       <HeroBanner />
       <Trending />
-      <Popular />
-      <TopRated />
       <Upcoming />
       <OnAir />
       <div style={{ height: 1000 }}></div>
